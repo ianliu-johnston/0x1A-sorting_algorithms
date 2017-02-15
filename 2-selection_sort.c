26dd5eb3510d3578d8803d5bdd131b4be6a64119
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-15 01:42:21 -0800

    Finished selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,12 +1,23 @@
 #include "sort.h"
 /**
-  * selection_sort - define function
-  * @array: describe argument
-  * @size: describe
-  * Return: 0 on success
+  * selection_sort - algorithm. slow.
+  * @array: input array
+  * @size: size of the input
   */
 void selection_sort(int *array, size_t size)
 {
-	array++;
-	size++;
+	size_t i, j, low, tmp;
+	int *head;
+
+	head = array;
+	for (i = 0; i < size; i++, head++)
+	{
+		for (low = i, j = i; j < size; j++)
+			if (array[j] < array[low])
+				low = j;
+		tmp = array[low];
+		array[low] = *head;
+		*head = tmp;
+		print_array(array, size);
+	}
 }
